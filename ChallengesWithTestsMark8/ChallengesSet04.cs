5c1b8de82a9fc82dc0f3402ad702aae4bf501b9c
Micah Ammons <micahammons@gmail.com>
2023-11-27 20:21:31 -0500

    finished week 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +7,128 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
-        }
+            int total = 0;
+            foreach (int num in numbers) 
+            {
+                if (num % 2 == 0)
+                {
+                    total += num;
+                }
+                if (num % 2 != 0) 
+                { 
+                    total -= num;
+                }
+            }
+            return total;
+
+            }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
-        }
+
+           return (Math.Min(Math.Min(Math.Min(str1.Length, str2.Length),str3.Length),str4.Length));
+                   
+         //   string lead;
+         //   if (str1.Length <= str2.Length) 
+         //   {
+         //      lead = str1;
+         //   }
+         //   else
+         //   {
+         //       lead = str2;
+         //   }
+         //   if (str3.Length <= lead.Length)
+         //   {
+         //       lead = str3;
+         //   }
+         //   if (str4.Length <= lead.Length)
+         //   {
+         //       lead = str4;
+         //   }return lead.Length;
+         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            return Math.Min(Math.Min(Math.Min(number1, number2),number3), number4);
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return ((sideLength1 + sideLength2) > sideLength3 && ((sideLength1 + sideLength3) > sideLength2 && ((sideLength2 + sideLength3) > sideLength1))) ? true : false;
+
+            //if ((sideLength1 + sideLength2) > sideLength3 && ((sideLength1 + sideLength3) > sideLength2 && ((sideLength2 + sideLength3) > sideLength1)))
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            double result;
+            return double.TryParse(input, out result);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int nullCount = 0;
+            int nonNullCount = 0;
+            foreach (object obj in objs) 
+            {
+               if (obj == null)
+                {
+                    nullCount++;
+                }
+               else if (obj != null) 
+                {
+                    nonNullCount++;
+                }
+            }return nonNullCount < nullCount;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0.0;
+            }
+            double evens = 0;
+            double countEvens = 0;
+            foreach (int num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    evens += num;
+                    countEvens++;
+                }
+                
+            }
+            if (countEvens == 0)
+            {
+                return 0;
+            }
+            return (evens / countEvens);
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
-        }
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("TRY AGAIN NERD MAKE IT A POSITIVE NUMBER OR I'M POSITIVE YOU A DUMB DUMB");
+            }
+            int final = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                final *= i;
+            }return final;
+        } 
     }
 }
